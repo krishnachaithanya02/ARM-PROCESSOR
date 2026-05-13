4531bca094561e5a8b379d5ac3b1ea4cd81bcdf4
krishna chaithanya <krishnachaithanya0202@gmail.com>
2026-05-13 12:33:31 +0530

    Create STM32F411RETX.c

---
+++ BAREMETAL PROGRAM/STM32F411RETX.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+
+#define RCC_AHB1ENR  (*(volatile uint32_t*)0x40023830)
+#define GPIOA_MODER (*(volatile uint32_t*)0x40020000)
+#define GPIOA_ODR (*(volatile uint32_t*)0x40020014)
+void delay(){
+	for(int i=0;i<500000;i++);
+
+}
+int main(void)
+{
+	RCC_AHB1ENR |=(1<<0);
+	GPIOA_MODER &= ~(3<<10);
+	GPIOA_MODER |=(1<<10);
+
+while(1)
+{
+	GPIOA_ODR |=(1<<5);
+	delay();
+		GPIOA_ODR &= ~(1<<5);
+		delay();
+}
+}
